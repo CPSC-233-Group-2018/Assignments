cb4821efe49f703ac875e33428a5205196d26b8f
descent098 <kieranw098@gmail.com>
2018-01-24 12:16:39 -0700

    updating widthdraw to handle overdraft amount limitation, has a symbol compile error

--- Assignment 2/BankAccount.java
+++ Assignment 2/BankAccount.java
@@ -10,19 +10,19 @@ public class BankAccount{
 
   public static void main(String[] args){
     /**
-    This function creates all the initial attributes needed for the class
+    This function calls and invokes all the methods
     */
 
-    //Test code
+    //Start of test code
     System.out.println("The current balance is: " + get_balance());//Test code to print current balance
-    widthdraw(10.12);
+    widthdraw(500);
     System.out.println("\nThe current balance is: " + get_balance() + "\nAnd it should be 89.88");//Test code to print current balance
     deposit(10.12);
     System.out.println("\nThe current balance is: " + get_balance() + "\nAnd it should be 100.0");//Test code to print current balance
 
-    System.out.println("The current overdraftAmount is: " + overdraftAmount);
+    System.out.println("\nThe current overdraftAmount is: " + overdraftAmount);
     set_overdraftAmount(200.00);
-    System.out.println("The current overdraftAmount is: " + overdraftAmount);
+    System.out.println("\nThe current overdraftAmount is: " + overdraftAmount);
     //End of test code
   }
   public static double get_balance() {
@@ -36,7 +36,18 @@ public class BankAccount{
     /**
     Method for widthdrawing from the balance of the account,takes a double as an argument
     */
-    balance -= amount;
+    do{
+      double overdraftDifference = (0.00-overdraftAmount);
+      double new_balance = balance - amount;
+      boolean bad_credit = false;
+      System.out.println("\nThe overdraftDifference is: " + overdraftDifference);
+
+    if (new_balance < overdraftDifference){
+      bad_credit = true;
+      System.out.println("Your too poor try again");
+      amount = 10;
+      }
+    } while (bad_credit == true);
   }
 
   public static void deposit(double amount){
