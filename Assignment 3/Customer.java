b640578dd2c12f903a95b588bb71841cb88311d2
William Chan <willchan789@gmail.com>
2018-01-31 10:59:30 -0700

    Added getter methods for name and id

--- Assignment 3/Customer.java
+++ Assignment 3/Customer.java
@@ -1,31 +1,39 @@
 
 public class Customer {
-	
+
 	public String name; //instance variables
 	public int customerID;
-	
+
 	public void setName(String input) { //method for name
 		name = input;
 	}
-	
+
 	public void setID(int input) { //method for ID
 		customerID = input;
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public int getID() {
+		return customerID;
+	}
+
 	public String toString() { //converts ID to string, not entirely sure about the return?
 		String strID = Integer.toString(customerID);
 		return strID;
 	}
-	
-	public Customer() { //constructor with no arguments 
-		
+
+	public Customer() { //constructor with no arguments
+
 	}
-	
+
 	Customer(String cusName, int cusID) { //constructor that takes name and ID
 		this.name = cusName;
 		this.customerID = cusID;
 	}
-	
+
 	void information() { //prints out info from constructor
 		System.out.println("Customer name: " + name + "." + "\nCustomer ID: " + customerID + ".");
 	}
