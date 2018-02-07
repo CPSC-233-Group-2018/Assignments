d92349453c5ab2c550bdbbcba653d78382cf061a
seth239 <seth.campbell2597@Gmail.com>
2018-02-06 21:27:34 -0700

    Tidied up and added more comments

--- Assignment 3/Customer.java
+++ Assignment 3/Customer.java
@@ -1,40 +1,63 @@
-
+/**
+ * Tutorial 6 Team 3:
+ * Seth Campbell, Kieran Woods, Rulan Lu, William Chan Jan 26, 2018
+ * -team assignment 3, Feb.6,2018-
+ *  	A customer class that interacts with the bankAccount class. 
+*/
 public class Customer {
 
 	public String name; //instance variables
 	public int customerID;
 
-	Customer() {		//default constructor
+	/** Default constructor */
+	Customer() {		
     name = "";
     customerID = 0;
-  }
-
-	Customer(Customer c) {      //copy constructor
+	}
+	
+	/** Copy constructor */
+	Customer(Customer c) {     
     name = c.name;
     customerID = c.customerID;
-  }
-
-	Customer(String cusName, int cusID) { //constructor that takes name and ID
+	}
+	
+	/** Constructor taking name and ID */
+	Customer(String cusName, int cusID) { 
 		name = cusName;
 		customerID = cusID;
 	}
 
-	public void setName(String input) { //method for name
+	/** sets name of customer
+	* @param name 
+	*/
+	public void setName(String input) { 
 		name = input;
 	}
 
-	public void setID(int input) { //method for ID
+	/** sets ID of customer
+	* @param ID number 
+	*/
+	public void setID(int input) { 
 		customerID = input;
 	}
 
+	/** retrieves name of customer
+	* @return name of customer
+	*/
 	public String getName() {
 		return name;
 	}
 
+	/** retrieves ID of customer
+	* @return ID of customer
+	*/
 	public int getID() {
 		return customerID;
 	}
 
+	/** Formats the name and ID of customer in a read-able way.
+	* @return formatted string of name and ID
+	*/
 	public String toString() { //converts Customer object into a string that can be printed.
 		String strID = Integer.toString(customerID);
 		return "Customer name: " + name + "." + "\nCustomer ID: " + strID + ".";
